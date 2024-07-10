3ed4b34aaf40fc84fb69d9df95323e1252088a7b
Adam Maxwell <maxwell7774@gmail.com>
2024-07-09 18:31:30 -0600

    IDK

--- init.lua
+++ init.lua
@@ -490,6 +490,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        tailwindcss = {},
         --
 
         lua_ls = {
